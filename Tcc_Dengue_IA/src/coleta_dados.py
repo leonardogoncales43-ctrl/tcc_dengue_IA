71871aae5f82cca07d58bcd67e57b6602f8ac943
leonardogoncales43-ctrl <leonardogoncales43@gmail.com>
2026-08-28 11:21:56 -0300

    Update coleta_dados.py

--- Tcc_Dengue_IA/src/coleta_dados.py
+++ Tcc_Dengue_IA/src/coleta_dados.py
@@ -1,29 +1,26 @@
 import os
-import requests
 import pandas as pd
 
 
-def coletar_dados_infodengue(codigo_ibge=3549904):
-    url = f"https://info.dengue.mat.br/api/alertcity?geocode={codigo_ibge}&disease=dengue&format=json&ew_start=1&ew_end=53&ey_start=2023&ey_end=2026"
+def buscar_dados_infodengue(codigo_ibge=3549904):
+    # Força a busca dos dados até o ano atual (2026)
+    url = f"https://infodengue.mat.br/api/alertcity?geocode={codigo_ibge}&disease=dengue&format=csv&ew_start=1&ew_end=53&ey_start=2023&ey_end=2026"
 
-    print("🌐 Baixando dados atualizados da API InfoDengue...")
-    response = requests.get(url)
+    pasta_src = os.path.dirname(os.path.abspath(__file__))
+    raiz_projeto = os.path.dirname(pasta_src)
+    caminho_destino = os.path.join(
+        raiz_projeto, "data", "raw", f"infodengue_{codigo_ibge}.csv"
+    )
 
-    if response.status_code == 200:
-        df = pd.DataFrame(response.json())
-        pasta_src = os.path.dirname(os.path.abspath(__file__))
-        raiz_projeto = os.path.dirname(pasta_src)
-        pasta_raw = os.path.join(raiz_projeto, "data", "raw")
-        os.makedirs(pasta_raw, exist_ok=True)
+    os.makedirs(os.path.dirname(caminho_destino), exist_ok=True)
 
-        caminho_saida = os.path.join(pasta_raw, f"infodengue_{codigo_ibge}.csv")
-        df.to_csv(caminho_saida, index=False)
-        print(f"✅ Dados da API salvos com sucesso em: {caminho_saida}")
-    else:
-        print(
-            f"❌ Erro ao acessar API InfoDengue: Status {response.status_code}"
-        )
+    df = pd.read_csv(url)
+    df.to_csv(caminho_destino, index=False)
+    print(
+        f"✅ Dados baixados da API InfoDengue. Última SE encontrada na API:"
+        f" {df['SE'].max()}"
+    )
 
 
 if __name__ == "__main__":
-    coletar_dados_infodengue(codigo_ibge=3549904)
+    buscar_dados_infodengue()
